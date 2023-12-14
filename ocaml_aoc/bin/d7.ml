1875e07f36b3799419430c179a5f1ef1d3cab045
nkxxll <niki.nice1203@gmail.com>
2023-12-14 21:01:27 +0100

    wip ocaml day 7

---
+++ ocaml_aoc/bin/d7.ml
@@ -0,0 +1,96 @@
+open Core
+open Ocaml_aoc
+
+(* advent of code 2023 day 7 *)
+
+type hand =
+  { cards : char list
+  ; score : int
+  ; bid : int
+  }
+
+let get_score cards =
+  (* get the card counts *)
+  let card_counts =
+    String.to_list cards
+    |> List.sort ~compare:Char.compare
+    |> List.group ~break:(fun a b -> not (Char.equal a b))
+    |> List.map ~f:(fun x -> List.length x)
+  in
+  (* get the score *)
+  (* get the card counts *)
+  match List.sort card_counts ~compare:Int.descending with
+  | [ 5 ] -> 7 (* straight flush *)
+  | [ 4; 1 ] -> 6 (* four of a kind *)
+  | [ 3; 2 ] -> 5 (* full house *)
+  | [ 3; 1; 1 ] -> 4 (* three of a kind *)
+  | [ 2; 2; 1 ] -> 3 (* two pair *)
+  | [ 2; 1; 1; 1 ] -> 2 (* one pair *)
+  | [ 1; 1; 1; 1; 1 ] -> 1 (* high card *)
+  | _ -> failwith "invalid input"
+;;
+
+let card_value ha =
+  match ha with
+  | 'A' -> 14
+  | 'K' -> 13
+  | 'Q' -> 12
+  | 'J' -> 11
+  | 'T' -> 10
+  | _ as card -> int_of_char card - int_of_char '0'
+;;
+
+let card_compare a b =
+  let rec aux a b =
+    match a, b with
+    | [], [] -> 0
+    | ha :: ta, hb :: tb ->
+      if Char.equal ha hb
+      then aux ta tb
+      else (
+        let a_val = card_value ha in
+        let b_val = card_value hb in
+        a_val - b_val)
+    | _ -> failwith "invalid input"
+  in
+  aux a b
+;;
+
+let compare a b =
+  let res =
+    if a.score <> b.score
+    then a.score - b.score
+    else card_compare a.cards b.cards
+  in
+  res
+;;
+
+let parse_card line =
+  let parts = String.split line ~on:' ' |> List.map ~f:String.strip in
+  match parts with
+  | [ cards; bid ] ->
+    { cards = String.to_list cards
+    ; score = get_score cards
+    ; bid = Int.of_string bid
+    }
+  | _ -> failwith "invalid input"
+;;
+
+let parse_cards_from_input input =
+  let rec aux acc = function
+    | [] -> acc
+    | h :: t ->
+      let card = parse_card h in
+      aux (card :: acc) t
+  in
+  aux [] input
+;;
+
+let () =
+  read_input 7
+  |> parse_cards_from_input
+  |> List.sort ~compare
+  |> List.rev
+  |> List.foldi ~f:(fun i acc hand -> acc + (hand.bid * (i + 1))) ~init:0
+  |> printf "Day 7 %d\n"
+;;
